b39e212c2548cfb3b9256df758c4926cf8d70cf5
Jonathan Roy <jroy@optimumweb.ca>
2015-12-31 12:20:18 -0500

    Style & Dev

--- hero.php
+++ hero.php
@@ -17,7 +17,9 @@
                     </div>
                 <?php elseif ( is_tax() ) : ?>
                     <div class="grid_12">
-                        <h1 class="title no-margin"><?php echo $wp_query->get_queried_object()->name; ?></h1>
+                        <h1 class="title no-margin">
+                            <?php echo sprintf(__("Syndics à %s", 'reseausyndic'), $wp_query->get_queried_object()->name); ?>
+                        </h1>
                     </div>
                 <?php endif; ?>
             </div>
