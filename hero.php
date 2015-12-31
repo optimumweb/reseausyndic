5690df72ed29318f7a6d68fe150fdfd5ae261bb5
Jonathan Roy <jroy@optimumweb.ca>
2015-12-31 12:11:20 -0500

    Style & Dev

--- hero.php
+++ hero.php
@@ -9,7 +9,7 @@
                 <div class="grid_6">
                     <h1 class="title no-margin">
                         <?php if ( is_tax() ) : ?>
-                        allo
+                            <?php global $wp_query; echo $wp_query->get_queried_object()->name; ?>
                             <?php $term = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy')); echo $term->name; ?>
                         <?php else : ?>
                             <?php echo get_the_title($post_id); ?>
