4bdd9aaecedf6bd9cce161a24d3cd903d14993ee
Jonathan Roy <jroy@optimumweb.ca>
2015-12-31 12:05:03 -0500

    Style & Dev

--- hero.php
+++ hero.php
@@ -7,7 +7,13 @@
                 <?php dynamic_sidebar("Front Page Hero"); ?>
                 <?php elseif ( is_page() ) : ?>
                 <div class="grid_6">
-                    <h1 class="title no-margin"><?php echo get_the_title($post_id); ?></h1>
+                    <h1 class="title no-margin">
+                        <?php if ( is_tax() ) : ?>
+                            <?php $term = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy')); echo $term->name; ?>
+                        <?php else : ?>
+                            <?php echo get_the_title($post_id); ?>
+                        <?php endif; ?>
+                    </h1>
                 </div>
                 <div class="grid_6">
                     <?php if ( get_post_meta($post_id, 'tagline', true) ) : ?>
