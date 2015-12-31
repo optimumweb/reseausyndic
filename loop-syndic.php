8fee4b770bec449b013acd11fe9f456d4ff4fd88
Jonathan Roy <jroy@optimumweb.ca>
2015-12-31 16:57:10 -0500

    Style & Dev

--- loop-syndic.php
+++ loop-syndic.php
@@ -3,7 +3,13 @@
 <?php wpbp_post_before(); ?>
 <article id="syndic-<?php the_ID(); ?>" class="syndic vcard" itemtype="http://schema.org/LocalBusiness">
     <?php wpbp_post_inside_before(); ?>
-    <h3 class="fn org"><?php the_title(); ?></h3>
+    <h3 class="fn org">
+        <?php if ( get_field('url') ) : ?>
+        <a class="url" href="<?php echo get_field('url'); ?>" target="_blank"><?php the_title(); ?></a>
+        <?php else : ?>
+        <?php the_title(); ?>
+        <?php endif; ?>
+    </h3>
     <ul class="contacts">
         <li class="adr" itemprop="address" itemtype="http://schema.org/PostalAddress">
             <span class="street-address" itemprop="streetAddress"><?php echo get_field('street_address'); ?></span>,
@@ -15,6 +21,9 @@
             <span class="tel" itemprop="telephone"><?php echo get_field('tel'); ?></span>
         </li>
     </ul>
+    <div class="description">
+        <?php the_content(); ?>
+    </div>
     <div class="clear"></div>
     <?php wpbp_post_inside_after(); ?>
 </article>
